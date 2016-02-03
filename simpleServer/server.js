fff3a2ee1d6c02a3a0d9a639360c21a096543a14
akshay <kinhikar.akshay32@gmail.com>
2016-02-03 13:27:46 +0530

    Simple server using node created

---
+++ simpleServer/server.js
@@ -0,0 +1,8 @@
+ var http = require('http');
+ http.createServer(function(req,res){
+ 	res.writeHead(200,{'Content-type' : 'text/plain'});
+ 	res.end('hello world');
+
+ }).listen(8000, '127.0.0.1');
+
+ console.log("server running on 8000, 127.0.0.1");
