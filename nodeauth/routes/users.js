f63256106ede0f75814866008064a4d0b7366195
akshay <kinhikar.akshay32@gmail.com>
2016-02-08 14:02:43 +0530

    server -- added user form validation

--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -18,14 +18,14 @@ router.get('/login', function(req, res, next){
 	});
 });
 
-route.post('/users/register', function(req,res,next){
+router.post('/register', function(req,res,next){
 	var name = req.body.name;
 	var email = req.body.email;
 	var userName = req.body.userName;
 	var password = req.body.password;
 	var password2 = req.body.password2;
 	var password2 = req.body.password2;
-});
+
 
 	if(req.file.profileImage){
 		console.log('Uploading Image');
@@ -39,7 +39,47 @@ route.post('/users/register', function(req,res,next){
 	}else{
 		//set default Image
 		var profileImageName = 'noimage.png'
-
 	}
 
+	req.checkBody('name', 'Name filed is required').notEmpty();
+	req.checkBody('email', 'email filed is required').notEmpty();
+	req.checkBody('email', 'email filed is required').isEmail();
+	req.checkBody('userName', 'userName filed is required').notEmpty();
+	req.checkBody('password', 'password filed is required').notEmpty();
+	req.checkBody('password2', 'password2 filed is required').notEmpty();
+	req.checkBody('password2', 'password2 filed is required').equals(req.body.password);
+
+	//check for password
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors : errors,
+			name : name,
+			email : email,
+			username : username,
+			password : password,
+			password2 : password2
+		});
+	}else{
+		var newUser = new User({
+			name : name,
+			email : email,
+			username : username,
+			password : password,
+			profileImage : profileImageName
+		}); 
+
+		//create user
+		// User.createUser(newUser, function(error, user){
+		// 	if(error) throw error;
+		// 	console.log(user); 
+		// });
+
+		req.flash('success');
+		res.location('/');
+		res.redirect('/');
+	}
+
+ });
 module.exports = router;
