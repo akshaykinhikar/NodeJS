e3472f338a9036181ef693f582c2aeba8cddeb2f
akshay <kinhikar.akshay32@gmail.com>
2018-01-13 15:47:51 +0530

    morgan added

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var morgan = require('morgan'); // middleware
 
 var app = express();
+
+
+//Middleware
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+   res.json('my name is akshay'); 
+});
 
 app.listen(3000, function(err) {
     if(err) throw err;
