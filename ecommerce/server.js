001f8278c4eed04182c54b55d2815e0c52b935ed
akshay <kinhikar.akshay32@gmail.com>
2018-01-15 23:23:45 +0530

    intermediate commit

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -10,6 +10,8 @@ var ejsMate = require('ejs-mate');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
 var secret = require('./config/secret');
 var User = require('./models/user');
@@ -52,6 +54,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   secret: secret.secretKey,
+  store: new MongoStore({url: secret.database, autoReconnect: true })
 }));
 app.use(flash());
 
