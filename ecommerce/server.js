4b5f9afd41d34caef4168cef876c2a72d6e0e947
akshay <kinhikar.akshay32@gmail.com>
2018-01-20 00:39:32 +0530

    login actions

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -52,6 +52,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  next();
+});
 app.use(flash());
 
 
